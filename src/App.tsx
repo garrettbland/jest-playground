bb54d475ed8fce1dfe35f4f9e9d7ac5a5c266921
Garrett Bland <gmorganbland@gmail.com>
2023-05-09 23:05:14 -0500

    feat: url encoding for sharing, tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './style.css'
+// import './style.css'
 import 'xterm/css/xterm.css'
 import { WebContainer } from '@webcontainer/api'
 import { files } from './files'
 import { useState, useEffect, useRef, SetStateAction } from 'react'
 import { Terminal } from 'xterm'
+import { FitAddon } from 'xterm-addon-fit'
 import Editor from '@monaco-editor/react'
+import { compressToEncodedURIComponent, decompressFromEncodedURIComponent } from 'lz-string'
 
 // let webContainerInstance: WebContainer
 
@@ -21,7 +23,12 @@ export const App = () => {
             convertEol: true,
             cursorBlink: false,
         })
+        const fitAddon = new FitAddon()
+
+        terminal.loadAddon(fitAddon)
+
         terminal.open(terminalRef.current as HTMLDivElement)
+        fitAddon.fit()
 
         terminal.writeln('Booting WebContainer...')
 
@@ -37,6 +44,20 @@ export const App = () => {
                 startDevServer(terminal)
             })
             .catch((err) => console.log(err))
+    }, [])
+
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search)
+        if (params.has('file') || params.has('test')) {
+            setFileValue(
+                decompressFromEncodedURIComponent(params.get('file') ?? '') ??
+                    '// Something went wrong parsing file contents from URL'
+            )
+            setTestValue(
+                decompressFromEncodedURIComponent(params.get('test') ?? '') ??
+                    '// Something went wrong parsing test file contents from URL'
+            )
+        }
     }, [])
 
     const bootContainer = async () => {
@@ -91,6 +112,61 @@ export const App = () => {
         writeFile(fileName, content)
     }
 
+    const handleShare = async (file: string, testFile: string) => {
+        const fileCode = compressToEncodedURIComponent(file)
+        const testCode = compressToEncodedURIComponent(testFile)
+        window.history.pushState({}, '', `?file=${fileCode}&test=${testCode}`)
+        await navigator.clipboard.writeText(window.location.href)
+        window.alert('copied URL to clipboard')
+    }
+
+    return (
+        <main className="overflow-hidden h-screen w-screen">
+            <nav className="flex items-center justify-between py-2 px-4">
+                <div className="">Jest Playground</div>
+                <button
+                    onClick={() => handleShare(fileValue, testValue)}
+                    className="bg-blue-500 hover:bg-blue-600 rounded px-2 py-1 text-white"
+                >
+                    Share
+                </button>
+            </nav>
+            <div className="flex flex-row space-x-4">
+                <div className="flex flex-col w-1/2">
+                    <Editor
+                        language="javascript"
+                        value={fileValue}
+                        onChange={(value = '') => handleFileInput('index.js', value, setFileValue)}
+                        theme="vs-dark"
+                        options={{
+                            minimap: {
+                                enabled: false,
+                            },
+                        }}
+                        className="h-[90%]"
+                    />
+                    <Editor
+                        language="javascript"
+                        value={testValue}
+                        onChange={(value = '') =>
+                            handleFileInput('index.test.js', value, setTestValue)
+                        }
+                        theme="vs-dark"
+                        options={{
+                            minimap: {
+                                enabled: false,
+                            },
+                        }}
+                        className="-mt-4"
+                    />
+                </div>
+                <div className="w-1/2">
+                    <div ref={terminalRef} className="h-screen bg-red-500" />
+                </div>
+            </div>
+        </main>
+    )
+
     return (
         <div className="container">
             <div className="editor">
@@ -121,6 +197,7 @@ export const App = () => {
                         },
                     }}
                 />
+                <button onClick={() => handleShare(fileValue, testValue)}>Share</button>
             </div>
             <div className="preview">
                 <div className="terminal" ref={terminalRef} />
